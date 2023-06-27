0d92142b3e8c8d8f57b4045cbc42a0f26ad5cfb6
mohamed ilyas karouata <ikarouata08@gmail.com>
2023-06-26 17:24:10 -0700

    python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# 2-safe_print_list_integers.py
+# 0-safe_print_list.py
 # Brennan D Baraban <375@holbertonschool.com>
 
 
-def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x elements of a list that are integers.
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
 
     Args:
         my_list (list): The list to print elements from.
@@ -14,11 +14,11 @@ def safe_print_list_integers(my_list=[], x=0):
         The number of elements printed.
     """
     ret = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             ret += 1
-        except (ValueError, TypeError):
-            continue
+        except IndexError:
+            break
     print("")
     return (ret)
--- 0x05-python-exceptions/tests/1-main.py
+++ 0x05-python-exceptions/tests/1-main.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-safe_print_integer_err = \
-    __import__('100-safe_print_integer_err').safe_print_integer_err
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
 value = 89
-has_been_print = safe_print_integer_err(value)
+has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
 value = -89
-has_been_print = safe_print_integer_err(value)
+has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
-value = "School"
-has_been_print = safe_print_integer_err(value)
+value = "Holberton"
+has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
-
